31809be7928d63476eb6eb38e86e7c0003bf4654
Markel Spellman <markel.spellman@gmail.com>
2020-08-05 19:49:00 -0400

    Save Venus after google lookup

--- navigation/screens/home/index.js
+++ navigation/screens/home/index.js
@@ -3,6 +3,8 @@ import { SafeAreaView, Text } from "react-native";
 import { container } from "@assets/styles";
 import VenueList from "@components/organisms/venue-list";
 import { fetchLocalVenues } from "@utils/helpers";
+import { OMNI_API_URL } from "@env";
+import axios from "axios";
 
 // TODO: create FriendActivityList (organism)
 // TODO: create VenueList (organism)
@@ -10,11 +12,31 @@ import { fetchLocalVenues } from "@utils/helpers";
 export default function HomeScreen() {
   const [venues, setVenues] = useState([]);
 
+  async function saveVenues(data) {
+    const venueList = JSON.stringify(data);
+    try {
+      console.log("Saving Venues to DB...");
+      await axios({
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        url: OMNI_API_URL + "/venue/new/batch",
+        data: {
+          venues: venueList,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     let data;
     async function fetchData() {
       data = await fetchLocalVenues();
       setVenues(data.results);
+
+      //save data to our db
+      await saveVenues(venues);
     }
     fetchData();
   }, []);
